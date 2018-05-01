84d046b8d73d87df7b3ff7dc0eed872b9856d82a
Darwin Guailla <darwinj94@hotmail.com>
2018-04-30 23:48:41 -0500

    termine de moficar el sidebar

--- src/app/layout/bs-component/bs-component.component.ts
+++ src/app/layout/bs-component/bs-component.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { routerTransition } from '../../router.animations';
 
 @Component({
     selector: 'app-bs-component',
     templateUrl: './bs-component.component.html',
-    styleUrls: ['./bs-component.component.scss']
+    styleUrls: ['./bs-component.component.scss'],
+    animations: [routerTransition()]
+    
 })
 export class BsComponentComponent implements OnInit {
     constructor() {}
